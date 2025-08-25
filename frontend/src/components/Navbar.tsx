0f889f06617e3c25763e39863b9608efe3fa69bd
tewodros-m <tewodrosmengie@gmail.com>
2025-08-25 20:04:42 +0300

    feat(navbar): show register and login only when not logged in

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,18 +1,39 @@
 import { Link } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+import type { RootState } from '../app/store';
+import { logout } from '../features/auth/authSlice';
 
 const Navbar = () => {
+  const dispatch = useDispatch();
+  const user = useSelector((state: RootState) => state.auth.user);
+
   return (
     <nav className='bg-primary-500 text-white px-6 py-3 flex justify-between items-center'>
       <Link to='/' className='text-2xl font-bold'>
         TalentHub
       </Link>
+
       <div className='flex gap-4 items-center'>
-        <Link to='/login' className='hover:underline'>
-          Login
-        </Link>
-        <Link to='/register' className='hover:underline'>
-          Register
-        </Link>
+        {!user ? (
+          <>
+            <Link to='/login' className='hover:underline'>
+              Login
+            </Link>
+            <Link to='/register' className='hover:underline'>
+              Register
+            </Link>
+          </>
+        ) : (
+          <>
+            <span className='font-medium'>Welcome, {user.name}</span>
+            <button
+              onClick={() => dispatch(logout())}
+              className='hover:underline'
+            >
+              Logout
+            </button>
+          </>
+        )}
       </div>
     </nav>
   );
