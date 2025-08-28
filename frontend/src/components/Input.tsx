0a74b87e58c6a5253f8828a91652c495a3c1c4c4
tewodros-m <tewodrosmengie@gmail.com>
2025-08-29 01:17:53 +0300

    feat(Input): add password input visibility toggle functionality

--- frontend/src/components/Input.tsx
+++ frontend/src/components/Input.tsx
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Eye, EyeOff } from 'lucide-react';
+import Button from './ui/Button';
 
 type InputProps = React.InputHTMLAttributes<HTMLInputElement> & {
   label?: string;
@@ -6,7 +8,10 @@ type InputProps = React.InputHTMLAttributes<HTMLInputElement> & {
 };
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
-  ({ label, error, className = '', id, ...props }, ref) => {
+  ({ label, error, className = '', id, type, ...props }, ref) => {
+    const [showPassword, setShowPassword] = useState(false);
+
+    const isPassword = type === 'password';
     return (
       <div className='w-full'>
         {label && (
@@ -17,11 +22,14 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
             {label}
           </label>
         )}
-        <input
-          ref={ref}
-          id={id}
-          {...props}
-          className={`w-full p-2 border rounded outline-none  bg-bg
+
+        <div className='relative'>
+          <input
+            ref={ref}
+            id={id}
+            type={isPassword && showPassword ? 'text' : type}
+            {...props}
+            className={`w-full p-2 border rounded outline-none bg-bg
               placeholder:text-gray-400
               ${
                 error
@@ -29,7 +37,20 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
                   : 'border-gray-300 focus:ring-2 focus:ring-primary-600'
               }
               ${className}`}
-        />
+          />
+
+          {/* Toggle icon only for password fields */}
+          {isPassword && (
+            <Button
+              type='button'
+              variant='custom'
+              onClick={() => setShowPassword((prev) => !prev)}
+              className='absolute right-3 top-1/2 -translate-y-1/2 text-gray-500 px-[6px] py-1 rounded'
+            >
+              {showPassword ? <EyeOff size={22} /> : <Eye size={22} />}
+            </Button>
+          )}
+        </div>
         {error && <p className='text-red-500 text-sm '>{error}</p>}
       </div>
     );
